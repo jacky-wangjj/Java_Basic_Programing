a525d9a61cff7d9613e24b108b6e0b6ee1078b61
jacky-wangjj <iot_wangjunjie@126.com>
2018-11-11 22:35:37 +0800

    实现冒泡排序的四种情况：从小到大，从大到小，先排定最小元素，先排定最大元素

--- src/main/java/SortAlgorithm/Bubble.java
+++ src/main/java/SortAlgorithm/Bubble.java
@@ -6,15 +6,76 @@ package main.java.SortAlgorithm;
 public class Bubble {
     /**
      * 冒泡排序
+     * 从小到大递增，先排最小元素。
      * 两两比较相邻元素，逆序则交换位置，直到没有元素交换为止
      * @param a
      */
     public void sort(int[] a) {
-        int flag = 0;//交换标志为0
         for(int i=0; i<a.length-1; i++) {//排序总次数
-            for(int j=a.length-1; j>i; j--) {//内循环控制排序的进行
-                if(a[j] < a[j-1]) {//相邻元素比较，逆序交换
+            int flag = 0;//交换标志为0
+            for(int j=a.length-1; j>i; j--) {//内循环控制排序的进行，先排定最小元素
+                if(a[j] < a[j-1]) {//相邻元素比较，逆序交换，从小到大递增
                     exch(a, j, j-1);
+                    flag = 1;//发生交换，将标志位置位
+                }
+            }
+            if (flag == 0)//本次排序没有发生交换，则排序完毕，终止操作
+                break;
+        }
+    }
+
+    /**
+     * 冒泡排序
+     * 从小到大递增，先排最大元素。
+     * 两两比较相邻元素，逆序则交换位置，直到没有元素交换为止
+     * @param a
+     */
+    public void sort2(int[] a) {
+        for(int i=1; i<a.length-1; i++) {//排序总次数
+            int flag = 0;//交换标志为0
+            for(int j=0; j<a.length-i; j++) {//内循环控制排序的进行，先排定最大元素
+                if(a[j+1] < a[j]) {//相邻元素比较，逆序交换，从小到大递增
+                    exch(a, j+1, j);
+                    flag = 1;//发生交换，将标志位置位
+                }
+            }
+            if (flag == 0)//本次排序没有发生交换，则排序完毕，终止操作
+                break;
+        }
+    }
+
+    /**
+     * 冒泡排序
+     * 从大到小递减，先排最小元素。
+     * 两两比较相邻元素，逆序则交换位置，直到没有元素交换为止
+     * @param a
+     */
+    public void sort3(int[] a) {
+        for (int i=0; i<a.length-1; i++) {//排序总次数
+            int flag =0;//交换标志位为0
+            for (int j=a.length-1; j>i; j--) {//内循环控制排序的进行，先排定最小元素
+                if (a[j] > a[j-1]) {//相邻元素比较，逆序交换，从大到小递减
+                    exch(a, j, j-1);
+                    flag = 1;//发生交换，将标志位置位
+                }
+            }
+            if (flag == 0)//本次排序没有发生交换，则排序完毕，终止操作
+                break;
+        }
+    }
+
+    /**
+     * 冒泡排序
+     * 从大到小递减，先排最大元素。
+     * 两两比较相邻元素，逆序则交换位置，直到没有元素交换为止
+     * @param a
+     */
+    public void sort4(int[] a) {
+        for (int i=1; i<a.length; i++) {//排序总次数
+            int flag =0;//交换标志位为0
+            for (int j=0; j<a.length-i; j++) {//内循环控制排序的进行，先排定最大元素
+                if (a[j+1] > a[j]) {//相邻元素比较，逆序交换，从大到小递减
+                    exch(a, j+1, j);
                     flag = 1;//发生交换，将标志位置位
                 }
             }
@@ -58,5 +119,17 @@ public class Bubble {
         s.resultPrint("before sort:", a);
         s.sort(a);
         s.resultPrint("after sort:", a);
+        int[] a2 = {9,2,5,6,3,2,1,10,4};
+        s.resultPrint("before sort2:", a2);
+        s.sort2(a2);
+        s.resultPrint("after sort2", a2);
+        int[] a3 = {9,2,5,6,3,2,1,10,4};
+        s.resultPrint("before sort3:", a3);
+        s.sort3(a3);
+        s.resultPrint("after sort3", a3);
+        int[] a4 = {9,2,5,6,3,2,1,10,4};
+        s.resultPrint("before sort4:", a4);
+        s.sort4(a4);
+        s.resultPrint("after sort4", a4);
     }
 }
